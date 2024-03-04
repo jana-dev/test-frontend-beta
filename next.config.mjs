35ae767a26ad900bf36366635cca4a6352e36bd0
Janaina Tavares <janixgg@outlook.com>
2024-03-03 21:36:03 -0300

    update json

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    target: 'experimental-serverless-trace',
-    basePath: '/test-frontend-beta',
     images:{
         domains: ['cdn.dummyjson.com'],
     }
